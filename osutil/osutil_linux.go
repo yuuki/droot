d1fdb61c59f86d08f814b255adcfb3a9ebffd364
y_uuki <yuki.tsubo@gmail.com>
2016-03-05 22:17:55 +0900

    Fix build failure

--- osutil/osutil_linux.go
+++ osutil/osutil_linux.go
@@ -3,11 +3,15 @@ package osutil
 import (
 	"errors"
 	"golang.org/x/sys/unix"
+	"os/exec"
 	"os/user"
 	"strconv"
+	"syscall"
 
 	"github.com/docker/libcontainer/system"
 	"github.com/yuuki1/go-group"
+
+	"github.com/yuuki1/droot/log"
 )
 
 func LookupGroup(id string) (int, error) {
@@ -62,16 +66,16 @@ func DropCapabilities(keepCaps map[uint]bool) error {
 		}
 		if err := unix.Prctl(unix.PR_CAPBSET_READ, uintptr(i), 0, 0, 0); err != nil {
 			// Regard EINVAL as the condition of loop finish.
-			if errno, ok := err.(os.Errno); ok && errno == unix.EINVAL {
+			if errno, ok := err.(syscall.Errno); ok && errno == unix.EINVAL {
 				break
 			}
 			return err
 		}
-		if err := unix.Prctl(syscall.PR_CAPBSET_DROP, uintptr(i), 0, 0, 0); err != nil {
+		if err := unix.Prctl(unix.PR_CAPBSET_DROP, uintptr(i), 0, 0, 0); err != nil {
 			// Ignore EINVAL since the capability may not be supported in this system.
-			if errno, ok := err.(os.Errno); ok && errno == unix.EINVAL {
+			if errno, ok := err.(syscall.Errno); ok && errno == unix.EINVAL {
 				continue
-			} else if errno, ok := err.(os.Errno); ok && errno == unix.EPERM {
+			} else if errno, ok := err.(syscall.Errno); ok && errno == unix.EPERM {
 				return errors.New("required CAP_SETPCAP capabilities")
 			} else {
 				return err
@@ -89,7 +93,7 @@ func DropCapabilities(keepCaps map[uint]bool) error {
 func Execv(cmd string, args []string, env []string) error {
 	name, err := exec.LookPath(cmd)
 	if err != nil {
-		return errwrap.Wrapff(err, "Not found %s: {{err}}", cmd)
+		return err
 	}
 
 	log.Debug("exec: ", name, args)
