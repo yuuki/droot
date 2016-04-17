b009102a669360e33d1eaba854c831cd1c74d619
y_uuki <yuki.tsubo@gmail.com>
2016-04-17 18:04:17 +0900

    Add TestResolveRootDir

---
+++ mounter/mounter_test.go
@@ -0,0 +1,15 @@
+package mounter
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestResolveRootDir(t *testing.T) {
+	_, err := ResolveRootDir("/path/to/notexist")
+	assert.Error(t, err)
+
+	_, err = ResolveRootDir("../")
+	assert.Error(t, err)
+}
