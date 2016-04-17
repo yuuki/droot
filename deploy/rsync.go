39d9f1be426c9021aba8a051333b443af7bc777b
y_uuki <yuki.tsubo@gmail.com>
2016-04-17 19:29:40 +0900

    Exclude bind-mounted directory by droot run

--- deploy/rsync.go
+++ deploy/rsync.go
@@ -1,12 +1,14 @@
 package deploy
 
 import (
+	"fmt"
 	"strings"
 
+	"github.com/yuuki/droot/mounter"
 	"github.com/yuuki/droot/osutil"
 )
 
-var RsyncDefaultOpts = []string{"-av", "--delete", "--exclude=/proc/*", "--exclude=/sys/*"}
+var RsyncDefaultOpts = []string{"-av", "--delete", "--exclude=/proc/", "--exclude=/sys/"}
 
 func Rsync(from, to string, arg ...string) error {
 	from = from + "/"
@@ -15,10 +17,22 @@ func Rsync(from, to string, arg ...string) error {
 		to = to + "/"
 	}
 
-	// TODO --exclude, --excluded-from
 	rsyncArgs := []string{}
 	rsyncArgs = append(rsyncArgs, RsyncDefaultOpts...)
+
+	// Exclude bind-mounted directory by droot run
+	mnt := mounter.NewMounter(to)
+	mounts, err := mnt.GetMountsRoot()
+	if err != nil {
+		return err
+	}
+	for _, m := range mounts {
+		mp := strings.TrimPrefix(m.Mountpoint, to)
+		rsyncArgs = append(rsyncArgs, fmt.Sprintf("--exclude=/%s", mp))
+	}
+
 	rsyncArgs = append(rsyncArgs, from, to)
+
 	if err := osutil.RunCmd("rsync", rsyncArgs...); err != nil {
 		return err
 	}
