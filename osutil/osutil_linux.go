33622d8c2b6a7cae503f31e9547a8ee28ec2544d
y_uuki <yuki.tsubo@gmail.com>
2017-06-11 03:12:03 +0900

    Fix build failure and remove 386 binary

--- osutil/osutil_linux.go
+++ osutil/osutil_linux.go
@@ -51,7 +51,7 @@ func LookupUser(id string) (int, error) {
 
 // Setuid sets the uid of the calling thread to the specified uid.
 func Setuid(uid int) (err error) {
-	_, _, e1 := syscall.RawSyscall(syscall.SYS_SETUID32, uintptr(uid), 0, 0)
+	_, _, e1 := syscall.RawSyscall(syscall.SYS_SETUID, uintptr(uid), 0, 0)
 	if e1 != 0 {
 		err = e1
 	}
@@ -60,7 +60,7 @@ func Setuid(uid int) (err error) {
 
 // Setgid sets the gid of the calling thread to the specified gid.
 func Setgid(gid int) (err error) {
-	_, _, e1 := syscall.RawSyscall(syscall.SYS_SETGID32, uintptr(gid), 0, 0)
+	_, _, e1 := syscall.RawSyscall(syscall.SYS_SETGID, uintptr(gid), 0, 0)
 	if e1 != 0 {
 		err = e1
 	}
--- script/build_release.sh
+++ script/build_release.sh
@@ -6,8 +6,6 @@ NAME="droot"
 
 ROOT=$(dirname $0)/..
 
-goxc -tasks='xc archive' -bc 'linux,!arm darwin' -d .
+goxc -tasks='xc archive' -bc 'linux,amd64,!arm darwin,amd64,!arm' -d .
 cp -p "$ROOT"/snapshot/linux_amd64/"$NAME" "$ROOT"/snapshot/"$NAME"_linux_amd64
-cp -p "$ROOT"/snapshot/linux_386/"$NAME" "$ROOT"/snapshot/"$NAME"_linux_386
 cp -p "$ROOT"/snapshot/darwin_amd64/"$NAME" "$ROOT"/snapshot/"$NAME"_darwin_amd64
-cp -p "$ROOT"/snapshot/darwin_386/"$NAME" "$ROOT"/snapshot/"$NAME"_darwin_386
