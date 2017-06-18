087c95aaac6f86ebd42efd342e36ac5416ff829e
y_uuki <yuki.tsubo@gmail.com>
2017-06-19 00:49:03 +0900

    Fix test

--- mounter/mount_test.go
+++ mounter/mount_test.go
@@ -6,8 +6,8 @@ import (
 
 func TestResolveRootDir(t *testing.T) {
 	_, err := ResolveRootDir("/path/to/notexist")
-	if err != nil {
-		t.Errorf("should not be error: %v", err)
+	if err == nil {
+		t.Error("should be error")
 	}
 
 	_, err = ResolveRootDir("../")
